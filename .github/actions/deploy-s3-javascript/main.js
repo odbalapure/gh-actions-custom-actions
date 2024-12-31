d027b5843b7f2f3d6e5d02227243120fc0e04353
Om Balapure <odbalapure@gmail.com>
2024-12-31 19:34:19 +0530

    Upload to S3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,14 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    // Get input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // Upload files
+    exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`);
+
     core.notice('This is a custom JS action!');
 }
 
